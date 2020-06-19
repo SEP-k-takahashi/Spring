ce116561bd4b7a023876d136050c0cb3fe3a8730
SEP-k-takahashi <k_takahashi@se-project.co.jp>
2020-06-19 13:08:47 +0900

    Category追加

--- AddressSP/src/main/java/com/example/demo/Category.java
+++ AddressSP/src/main/java/com/example/demo/Category.java
@@ -1,5 +1,43 @@
 package com.example.demo;
 
-public class Category {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+@Entity
+@Table(name="category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long categoryid;
+    private String categoryname;
+
+    public long getCategoryid() {
+        return categoryid;
+    }
+    public void setCategoryid(long categoryid) {
+        this.categoryid = categoryid;
+    }
+    public String getCategoryname() {
+        return categoryname;
+    }
+    public void setCategoryname(String categoryname) {
+        this.categoryname = categoryname;
+    }
+
+    @OneToMany(mappedBy="categoryid", cascade=CascadeType.ALL)
+    private List<User> user;
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public void setEquipments(List<User> user) {
+        this.user = user;
+    }
 }
